bcfd82059ad6f3d506954d480787fc7293417c18
SengSerany <cranyseng@gmail.com>
2020-07-13 12:22:19 +0200

    customize header color in admin theme panel (API Customize)

--- wp-content/themes/monBlog/functions.php
+++ wp-content/themes/monBlog/functions.php
@@ -1,6 +1,7 @@
 <?php
 
     require_once('walkers/CommentWalker.php');
+    require_once('options/apparence.php');
 
     function my_theme_support () {
         add_theme_support("title-tag");
@@ -17,8 +18,10 @@
         wp_register_style("bootstrap", 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css');
         wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
         wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', [], false, true);
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
+        if (!is_customize_preview()) {
+            wp_deregister_script('jquery');
+            wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
+        }
         wp_enqueue_style('bootstrap');
         wp_enqueue_script('bootstrap');
     }
